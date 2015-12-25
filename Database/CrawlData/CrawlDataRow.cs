de2e2cce1c338f4ec551e36c0ff0a8beb43e59d8
atifsheikh <atif.sheikh87@gmail.com>
2015-12-25 04:51:15 +0100

    DB structure draft 1

---
+++ Database/CrawlData/CrawlDataRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneKey.Database.CrawlData
+{
+    public class CrawlDataRow : Concept
+    {
+        public string Value;
+        public string listing_url;
+    }
+}
